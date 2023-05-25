fd7a955dfcf6403425d5f70cd3af1f168ec71366
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-05-25 14:54:17 -0400

    cors 2

--- index.mjs
+++ index.mjs
@@ -6,24 +6,9 @@ import express from "express";
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({origin: ['http://localhost:3000', 'http://127.0.0.1:3000']}));
 app.use(express.json());
 app.use("/rooms", rooms);
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use((err, _req, res, next) => {
   res.status(500).send("Uh oh! An unexpected error occured.")
